f8826d4a73905487fd74c5339ae259e51a6fdfba
kirkby <kirkby@neighbor.com>
2021-02-03 21:18:44 -0700

    progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -36,3 +36,8 @@ const func2a = () => { return "hi" }
 const func2b = () => {
   return "hi"
 };
+
+console.log(`1a: ${func1a()}`)
+console.log(`1b: ${func1b()}`)
+console.log(`2a: ${func2a()}`)
+console.log(`2b: ${func2b()}`)
