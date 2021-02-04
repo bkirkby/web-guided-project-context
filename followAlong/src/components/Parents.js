5f223a49ccd38d1d51a344e184d44370bd632e5c
kirkby <kirkby@neighbor.com>
2021-02-03 18:59:51 -0700

    progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import { familyContext } from '../App';
+
 const Parents = props => {
   return (
     <section className="parents">
-      {props.family.parents.map(p => (
-        <div className="person" key={p.name}>
-          <img width="150" src={p.img} alt={p.name} />
-          <strong>{p.name}</strong>
-        </div>
-      ))}
+      <familyContext.Consumer>
+        {props.family.parents.map(p => (
+          <div className="person" key={p.name}>
+            <img width="150" src={p.img} alt={p.name} />
+            <strong>{p.name}</strong>
+          </div>
+        ))}
+      </familyContext.Consumer>
     </section>
   );
 };
