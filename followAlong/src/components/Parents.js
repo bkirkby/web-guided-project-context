986122ff97663ea81136621dcee95b1425604cdd
kirkby <kirkby@neighbor.com>
2021-02-03 21:08:25 -0700

    progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -10,15 +10,12 @@ class Parents extends React.Component {
       <section className="parents">
         <familyContext.Consumer>
           {family => {
-            console.log('bk: PArents.js: render: family: ', family)
-            {
-              family.parents.map(p => (
-                <div className="person" key={p.name}>
-                  <img width="150" src={p.img} alt={p.name} />
-                  <strong>{p.name}</strong>
-                </div>
-              ))
-            }
+            family.parents.map(p => (
+              <div className="person" key={p.name}>
+                <img width="150" src={p.img} alt={p.name} />
+                <strong>{p.name}</strong>
+              </div>
+            ))
           }}
         </familyContext.Consumer>
       </section>
