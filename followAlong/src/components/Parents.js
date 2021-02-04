9ed17acc96c815e13fae82ed743459cd297fa9bd
kirkby <kirkby@neighbor.com>
2021-02-03 20:20:27 -0700

    progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const Parents = props => {
-  return (
-    <section className="parents">
-      {props.family.parents.map(p => (
-        <div className="person" key={p.name}>
-          <img width="150" src={p.img} alt={p.name} />
-          <strong>{p.name}</strong>
-        </div>
-      ))}
-    </section>
-  );
+// const Parents = props => {
+class Parents extends React.Component {
+  render() {
+    return (
+      <section className="parents">
+        {props.family.parents.map(p => (
+          <div className="person" key={p.name}>
+            <img width="150" src={p.img} alt={p.name} />
+            <strong>{p.name}</strong>
+          </div>
+        ))}
+      </section>
+    );
+  }
 };
 
 export default Parents;
