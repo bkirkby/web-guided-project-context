065adc7cf51e962eeafb91d65d7845afd84672b4
kirkby <kirkby@neighbor.com>
2021-02-03 18:58:13 -0700

    progress

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -2,11 +2,12 @@ import React, { useContext } from "react";
 
 import { familyContext } from '../App';
 
-const Siblings = (props) => {
-  const [family] = useContext(familyContext);
+const Siblings = () => {
+  const family = useContext(familyContext);
+
   return (
     <section className="parents">
-      {props.family.siblings.map((p) => (
+      {family.siblings.map((p) => (
         <div className="person" key={p.name}>
           <img width="150" src={p.img} alt={p.name} />
           <strong>{p.name}</strong>
