ad567b42156ae135ae79dced7a78d75d3b63db68
kirkby <kirkby@neighbor.com>
2021-02-03 19:01:50 -0700

    progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -6,12 +6,22 @@ const Parents = props => {
   return (
     <section className="parents">
       <familyContext.Consumer>
-        {props.family.parents.map(p => (
+        {family => {
+          return <>
+            {family.parents.map(p => (
+              <div className="person" key={p.name}>
+                <img width="150" src={p.img} alt={p.name} />
+                <strong>{p.name}</strong>
+              </div>
+            ))}
+          </>
+        }}
+        {/* {props.family.parents.map(p => (
           <div className="person" key={p.name}>
             <img width="150" src={p.img} alt={p.name} />
             <strong>{p.name}</strong>
           </div>
-        ))}
+        ))} */}
       </familyContext.Consumer>
     </section>
   );
