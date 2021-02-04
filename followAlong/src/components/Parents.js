21f6eceea5c5a532dfd1bab2e734b58c2f807049
kirkby <kirkby@neighbor.com>
2021-02-03 19:04:31 -0700

    progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -2,29 +2,32 @@ import React from 'react';
 
 import { familyContext } from '../App';
 
-const Parents = () => {
-  return (
-    <section className="parents">
-      <familyContext.Consumer>
-        {family => {
-          return <>
-            {family.parents.map(p => (
-              <div className="person" key={p.name}>
-                <img width="150" src={p.img} alt={p.name} />
-                <strong>{p.name}</strong>
-              </div>
-            ))}
-          </>
-        }}
-        {/* {props.family.parents.map(p => (
+// const Parents = () => {
+class Parents extends React.Component {
+  render() {
+    return (
+      <section className="parents">
+        <familyContext.Consumer>
+          {family => {
+            return <>
+              {family.parents.map(p => (
+                <div className="person" key={p.name}>
+                  <img width="150" src={p.img} alt={p.name} />
+                  <strong>{p.name}</strong>
+                </div>
+              ))}
+            </>
+          }}
+          {/* {props.family.parents.map(p => (
           <div className="person" key={p.name}>
             <img width="150" src={p.img} alt={p.name} />
             <strong>{p.name}</strong>
           </div>
         ))} */}
-      </familyContext.Consumer>
-    </section>
-  );
+        </familyContext.Consumer>
+      </section>
+    );
+  }
 };
 
 export default Parents;
