299acc398fd16e3eb797b724d616c6c455c2c225
kirkby <kirkby@neighbor.com>
2021-03-31 21:03:45 -0600

    progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -1,16 +1,21 @@
 import React from 'react';
+
+import familyContext from '../contexts/familyContext';
+
 // refactor as class
 // const Parents = (props) => {
 class Parents extends React.Component {
   render() {
     return (
       <section className="parents">
-        {this.props.family.parents.map(p => (
-          <div className="person" key={p.name}>
-            <img width="150" src={p.img} alt={p.name} />
-            <strong>{p.name}</strong>
-          </div>
-        ))}
+        <familyContext.Consumer>
+          {this.props.family.parents.map(p => (
+            <div className="person" key={p.name}>
+              <img width="150" src={p.img} alt={p.name} />
+              <strong>{p.name}</strong>
+            </div>
+          ))}
+        </familyContext.Consumer>
       </section>
     );
   }
