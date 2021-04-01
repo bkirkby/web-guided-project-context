69de60b286801ff8d5e207b1e6ed29489ab9cba8
kirkby <kirkby@neighbor.com>
2021-03-31 19:50:54 -0600

    progress

--- review/src/App.js
+++ review/src/App.js
@@ -4,7 +4,12 @@ const App = (props) => {
   const [score, setScore] = useState([]);
 
   useEffect(() => {
-
+    // update state
+    // api calls
+    setScore([
+      { name: 'ann', first: 8, second: 12 },
+      { name: 'biran', first: 7, score: 9 }
+    ])
   }, []);
 
   return (<h1>Hello World</h1>);
